a6c060a93d6f451e932fd9da34b5be489409c9ab
Mukul Gaikwad <mukul.gaikwad27@gmail.com>
2016-11-16 16:07:35 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -7,7 +7,7 @@ int main()
     printf("Enter an integer: ");
     scanf("%d",&n);
 
-    // show error if the user enters a negative integer
+    
     if (n < 0)
         printf("Error! Factorial of a negative number doesn't exist.");
 
@@ -15,7 +15,7 @@ int main()
     {
         for(i=1; i<=n; ++i)
         {
-            factorial *= i;              // factorial = factorial*i;
+            factorial *= i;              
         }
         printf("Factorial of %d = %llu", n, factorial);
     }
